c5b55d0e6cd38960f5d2684d1981c83054ee92e4
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2022-09-13 12:04:40 -0700

    Print putchar with new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: Always 0 (to be successful)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Task 0 - program that prints _putchar follower by a new line
