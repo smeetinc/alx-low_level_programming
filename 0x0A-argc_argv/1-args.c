d5276b90dd39a86e044359daab6208c23920658c
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2022-10-04 08:44:49 -0700

    print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  main - prints the number of arguenments passed to it
+ *  @argc: arguemnet count
+ *  @argv: pointer arg variable
+ *  Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
