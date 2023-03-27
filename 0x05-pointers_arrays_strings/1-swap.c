799718c4b913a808baf13806dad98a889d24771b
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-03-27 08:19:58 -0700

    function to swap two integers value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - function to swap two integers
+ * @a: params 1
+ * @b: params 2
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
