01fc758d2206dc39c25ee39899cec94a27bf6d2a
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-03-30 07:39:59 -0700

    Function to reverse array content

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+/**
+ * reverse_array - Function to reverse array content
+ * @a: function params
+ * @n: second params
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, i, j;
+
+	i = 0;
+	j = n - 1;
+	while (a < b)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		i++;
+		j--;
+	}
+}
