7d0ab1f5bea6fcb132373418f172c24014881b99
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-03-30 13:21:22 -0700

    Task 5 to change string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+ * string_toupper - function to change string to uppercase
+ * @s: func input
+ * Return: pointer to string input
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
