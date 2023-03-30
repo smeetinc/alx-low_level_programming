c19b5555ea46049186861eefe1b6932da1c4c44f
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-03-30 16:40:15 -0700

    Task 7 fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,15 +11,15 @@ char *leet(char *s)
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; n[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == s1[j])
+			if (s[i] == s1[j])
 			{
-				n[i] = s2[j];
+				s[i] = s2[j];
 			}
 		}
 	}
-	return (n);
+	return (s);
 }
