eb002a90634bc61145f1063a8dfc891a46f791cc
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-04-03 13:01:15 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+/**
+ * _strpbrk - function to search for set of bytes
+ * @s: func params 1
+ * @accept: func params 2
+ * Return: pointer to @s or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+			j++;
+		}
+		i++;
+	}
+	return ('\0');
+}
