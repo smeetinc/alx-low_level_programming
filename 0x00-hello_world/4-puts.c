7a02368eb1af6325ff8ea5a53420a8111f5c96cf
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-04-16 17:50:49 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Description: using put function to print text
+ * Return: 0 to exit
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
