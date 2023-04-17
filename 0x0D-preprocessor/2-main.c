1b9db1afaed0d12fb9bdf7d017b2973d55ad8c80
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-04-17 09:16:49 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * Description: program to print name of file
+ * Return: 0 to exit
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
