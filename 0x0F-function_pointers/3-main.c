b55fd5570f4b03a4b4f4220c733a1e0c43385d00
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-04-19 17:56:53 -0700

    Task 3 bug fix v2

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,7 +14,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2;
-	char *func;
+	char *operator;
 
 	if (argc != 4)
 	{
@@ -25,16 +25,16 @@ int main(int argc, char *argv[])
 	func = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_op_func(func) == NULL || func[1] != '\0')
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*func == 47 || *func == 37) && num2 == 0)
+	if ((*operator == 47 || *operator == 37) && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(func)(num1, num2));
+	printf("%d\n", get_op_func(operator)(num1, num2));
 	return (0);
 }
