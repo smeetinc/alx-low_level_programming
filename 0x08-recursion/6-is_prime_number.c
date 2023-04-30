bbf2ea496e4917c56d04369eaf4c782f61d0841a
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-04-30 09:17:08 -0700

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * prime_check - func for prime control
+ * @n: func param 1
+ * @c: func param 2
+ * Return: 1 || 0
+ */
+
+int prime_check(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + prime_check(n, c + 1));
+}
+
+/**
+ * is_prime_number - check if an input is a prime number
+ * @n: func param
+ * Return: 1 || 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (0);
+	else if (n < 0)
+		return (0);
+	else if (n == 1)
+		return (0);
+	else
+		return (is_prime(n, 2));
+}
