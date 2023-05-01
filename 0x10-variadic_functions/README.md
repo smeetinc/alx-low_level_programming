6259ffebc4d329d2414cd0b7ce6fa410f7f55d47
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-05-01 12:21:08 -0700

    Task 0

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+# Tasks on 0x10-variadic_functions done by Oluwaseyi Smith Bayode
+
+- 5/1/2023
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef _VARIADIC_FUNCTIONS
+#define _VARIADIC_FUNCTIONS
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
