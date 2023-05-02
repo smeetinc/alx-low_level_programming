43c139d1eb9eba9b982bb708b7576a99b68c3165
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-05-02 09:02:48 -0700

    Task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - functionto print all
+ * @format: func param
+ * Return: void value
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	unsigned int i = 0, j, a = 0;
+	char *str, const char type_arg[] = "cifs";
+
+	va_start(valist, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (type_arg[j])
+		{
+			if (format[i] == type_arg[j] && a)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(valist, int)), a = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(valist, int)), a = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(valist, double)), a = 1;
+				break;
+			case 's':
+				str = va_arg(valist, char *), a = 1;
+				if (!str)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+		} i++;
+	}
+	printf("\n"), va_end(valist);
+}
