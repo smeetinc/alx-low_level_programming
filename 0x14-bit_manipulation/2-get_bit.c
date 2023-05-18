eccf928f686c72c073db206a9ec06d86aaa29489
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-05-18 16:08:20 -0700

    Task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * get_bit - func to return val of a bit at index
+ * @n: func param
+ * @index: func param 2
+ * Return: bit on success
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == 0 && index < 64)
+		return (0);
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+	return (-1);
+}
