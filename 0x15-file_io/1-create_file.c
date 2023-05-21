d917e556d2c25f470f4f30fd7b7ac42f5858065c
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-05-21 08:51:40 -0700

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - func to create file
+ * @filename: name of file
+ * @text_content: content of file
+ * Return: 1 || -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_des;
+	int numlets;
+	int perm;
+
+	if (!filename)
+		return (-1);
+	file_des = open(filename, 0_CREAT | 0_WRONGLY | 0_TRUNC, 0600);
+	if (file_des == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	for (numlets = 0; text_content[numlets]; numlets++)
+		;
+	perm = write(file_des, text_content, numlets);
+	if (perm == -1)
+		return (-1);
+	close(file_des);
+	return (1);
+}
