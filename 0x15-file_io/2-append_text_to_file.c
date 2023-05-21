ba3e19c7ec946a4efb4a754493a30c1166c8b9bf
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-05-21 10:33:22 -0700

    Fix bug

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		for (numlets = 0; text_content[numlets]; numlets++)
 			;
-		perm write(file_des, text_content, numlets);
+		perm = write(file_des, text_content, numlets);
 		if (perm == -1)
 			return (-1);
 	}
