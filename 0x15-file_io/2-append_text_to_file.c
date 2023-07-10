742fa870f3b9e5498856aaaf9b2aaa1c969c0726
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-07-10 15:47:43 -0700

    Task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * append_text_to_file - append text func
+ * @filename: name of file
+ * @text_content: appened text
+ * Return: 1 || -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, numletrs, rwr;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		for (numletrs = 0; text_conetnt[numletrs]; numletrs++)
+			;
+		rwr = write(fd, text_conetnt, numletrs);
+		if (rwr == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
