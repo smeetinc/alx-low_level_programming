a8d1ab6696ae42b2d9c97ed8da2760db8b73a5d4
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-08-17 07:31:03 -0700

    Task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds new node at the head
+ * @head: list head
+ * @n: data in list
+ * Return: addr of new element
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *h;
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->prev = NULL;
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
+	newNode->next = h;
+	if (h != NULL)
+		h->prev = newNode;
+	*head = newNode;
+	return (newNode);
+}
